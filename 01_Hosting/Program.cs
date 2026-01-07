4d072f719682e7a891885514e46e5bfaff50d2bc
Emanuel dos Santos <ernelemanuel10@hotmail.com>
2026-01-07 15:33:42 +0100

    feat: first Program.cs using .NET Host

--- 01_Hosting/Program.cs
+++ 01_Hosting/Program.cs
@@ -6,13 +6,11 @@ namespace _01_Hosting
     {
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
             using IHost host = Host.CreateDefaultBuilder(args).Build();
-            Console.WriteLine("Hello, World!");
+            
             await host.StartAsync();
             Console.WriteLine("Hello, World!");
-            await host.StopAsync();
-            Console.WriteLine("Hello, World!");
+            await host.StopAsync();   
         }
     }
 }

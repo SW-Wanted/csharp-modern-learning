19ef2484fb8491fa6d6a763fbde4baa65b0bd382
Emanuel dos Santos <ernelemanuel10@hotmail.com>
2026-01-07 15:27:43 +0100

    Add project files.

---
+++ 01_Hosting/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace _01_Hosting
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            using IHost host = Host.CreateDefaultBuilder(args).Build();
+            Console.WriteLine("Hello, World!");
+            await host.StartAsync();
+            Console.WriteLine("Hello, World!");
+            await host.StopAsync();
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

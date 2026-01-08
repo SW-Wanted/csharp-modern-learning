69c72d71f7d47f79f12884bccbd5b4412018d426
Emanuel dos Santos <ernelemanuel10@hotmail.com>
2026-01-08 21:43:33 +0100

    Create a simple App class

---
+++ 01_Hosting/App.cs
@@ -0,0 +1,14 @@
+﻿namespace _01_Hosting
+{
+    public class App
+    {
+        public async Task RunAsync()
+        {
+            Console.WriteLine("App está a executar...");
+
+            await Task.Delay(1000);
+
+            Console.WriteLine("App terminou.");
+        }
+    }
+}

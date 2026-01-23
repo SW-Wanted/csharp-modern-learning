85831941ee69a9cd27d65404400dc616a54a000d
Emanuel dos Santos <ernelemanuel10@hotmail.com>
2026-01-24 00:23:14 +0100

    feat: adding Logging features

--- 02_Logging/Program.cs
+++ 02_Logging/Program.cs
@@ -10,20 +10,42 @@ namespace _02_Logging
     {
         static async Task Main(string[] args)
         {
+            // Configuração do Logger global Serilog
             Log.Logger = new LoggerConfiguration()
-               .MinimumLevel.Debug()
-               .Enrich.FromLogContext()
-               .WriteTo.Console()
-               .WriteTo.File("logs/myapp.log", rollingInterval: RollingInterval.Day)
-               .CreateLogger();
+                .MinimumLevel.Debug() // nível mínimo global
+                .Enrich.FromLogContext() // permite BeginScope e CorrelationId
+                .WriteTo.Console()       // console para desenvolvimento
+                .WriteTo.File("logs/log.json", rollingInterval: RollingInterval.Day, outputTemplate: "{Timestamp:O} [{Level}] {Message}{NewLine}{Properties}{Exception}")
+                .CreateLogger();
 
             using IHost host = Host.CreateDefaultBuilder(args)
-                .UseSerilog()
+                .UseSerilog() // substitui logger interno do Host pelo Serilog
+                .ConfigureLogging((context, logging) =>
+                {
+                    logging.ClearProviders();
+
+                    if (context.HostingEnvironment.IsDevelopment())
+                    {
+                        logging.AddConsole(options => options.IncludeScopes = true);
+                        logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                    }
+                    else
+                    {
+                        logging.AddJsonConsole(options => options.IncludeScopes = true);
+                        logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Information);
+                    }
+
+                    // Filtros por namespace/categoria
+                    logging.AddFilter("Microsoft", Microsoft.Extensions.Logging.LogLevel.Warning);
+                    logging.AddFilter("System", Microsoft.Extensions.Logging.LogLevel.Warning);
+                    logging.AddFilter("LoggingLab.Services", Microsoft.Extensions.Logging.LogLevel.Trace);
+                })
                 .ConfigureServices(services =>
                 {
                     services.AddSingleton<App>();
                     services.AddSingleton<LoggingDemoService>();
-                }).Build();
+                })
+                .Build();
 
             await host.Services.GetRequiredService<App>().RunAsync();
         }
